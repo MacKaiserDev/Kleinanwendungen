fad0aa7b147b4ae83fa09a3fec72438c0ed9add0
StefanKSSJ <Stefankaiser2@gmx.net>
2021-06-01 07:33:06 +0200

    JSON Beispiel

---
+++ Json-Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Json_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Sabrina = new Person()
+            {
+                Alter = 32,
+                Vornahme = "Sabrina",
+                Nachname = "Mustermann",
+                Sprachen = new List<string> { "Deutsch", "Englisch" }
+            };
+
+            string Json=JsonConvert.SerializeObject(Sabrina,Formatting.Indented);
+            Console.WriteLine(Json);
+            //Ausgabe
+            //{
+            //    "Vornahme": "Sabrina",
+            //    "Nachname": "Mustermann",
+            //    "Alter": 32,
+            //    "Sprachen": [
+            //            "Deutsch",
+            //            "Englisch"
+            //            ]
+            // }
+
+            Person newPerson = JsonConvert.DeserializeObject<Person>(Json);
+            Console.WriteLine(newPerson.Vornahme);
+            foreach (var item in newPerson)
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public string Vornahme { get; set; }
+        public string Nachname { get; set; }
+        public int Alter { get; set; }
+
+        public List<string> Sprachen { get; set; }
+    }
+
+}
