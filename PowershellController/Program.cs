da5b72ed77e31f09615427241110c5799400136e
StefanKSSJ <Stefankaiser2@gmx.net>
2021-06-01 06:40:11 +0200

    Initialpost

---
+++ PowershellController/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using PowerShell = System.Management.Automation.PowerShell;
+
+namespace PowershellController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //unter.net 4.x ist die system.managemente automatation nur bis Version 5 lauffähig. höhere Versionen erfordern .net Core 5.x.
+            //https://www.nuget.org/packages/Microsoft.PowerShell.5.ReferenceAssemblies/ 
+
+            using (PowerShell powershell = PowerShell.Create().AddCommand("get-process"))
+            {
+                Console.WriteLine("Process              HandleCount");
+                Console.WriteLine("--------------------------------");
+
+                // Invoke the command synchronously and display the
+                // ProcessName and HandleCount properties of the
+                // objects that are returned.
+                foreach (PSObject result in powershell.Invoke())
+                {
+                    Console.WriteLine(
+                                "{0,-50} {1}",
+                                result.Members["ProcessName"].Value,
+                                result.Members["HandleCount"].Value);
+                }
+            }
+
+            System.Console.WriteLine("Hit any key to exit...");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ WinHostsWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace WinHostsWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Server_IP, Server_Name;
+
+            Console.WriteLine("David Server IP im Format eingeben. Beispiel: 192.168.23.145");
+            Server_IP=Console.ReadLine();
+
+            Console.WriteLine("DNS Namen des David Servers eingeben. Beispiel: David.poltergeist.local");
+            Server_Name = Console.ReadLine();
+
+            string Eintrag = Server_IP + " " + Server_Name;
+            //string path = @"C:\Windows\System32\drivers\etc\hosts";
+            string path = @"C:\Users\stefa\Desktop\hosts";
+            FileStream fs = File.Open(path, FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            StreamWriter W = File.AppendText(path);
+
+            W.WriteLine(Eintrag);
+            //sw.WriteLine(Eintrag);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
